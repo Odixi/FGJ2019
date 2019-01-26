7016c0ac025f1cbe13b908093b1a4cb76c2939e0
Odixi <vz4ev4r@gmail.com>
2019-01-27 01:36:10 +0200

    GameOver

---
+++ Assets/Scrips/Wifi.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wifi : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+}
