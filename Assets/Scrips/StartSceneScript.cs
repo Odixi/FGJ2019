43d8ee77cd98b7a03df32605c35b2c4213d29952
Odixi <vz4ev4r@gmail.com>
2019-01-27 01:20:27 +0200

    Alku scane

---
+++ Assets/Scrips/StartSceneScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartSceneScript : MonoBehaviour
+{
+    public List<Sprite> images;
+    public List<float> times;
+
+    private Image im;
+
+    private int index = 0;
+    private float time = 0;
+
+    private void Start()
+    {
+        im = GetComponentInChildren<Image>();
+        im.sprite = images[index];
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if (time > times[index])
+        {
+            index++;
+            if (index >= times.Count)
+            {
+                SceneManager.LoadScene("Lobby");
+                return;
+            }
+            im.sprite = images[index];
+        }
+    }
+
+}
