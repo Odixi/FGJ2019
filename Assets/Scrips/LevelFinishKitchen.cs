d2e838aa8ac36f4334c4b2cc5ea74fd7444a7e0f
Vaintti <antti.jk.vainikka@gmail.com>
2019-01-26 20:01:00 +0200

    Continue on Kitchen scene and modify build settings to include scenes

--- Assets/Scrips/LevelFinishKitchen.cs
+++ Assets/Scrips/LevelFinishKitchen.cs
@@ -3,20 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelFinishLobby : MonoBehaviour
+public class LevelFinishKitchen : MonoBehaviour
 {
+
     public Animator animator;
 
     void Update()
     {
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Ended"))
         {
-            SceneManager.LoadScene("Kitchen");
+            SceneManager.LoadScene("TestScene");
         }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        animator.SetFloat("Speed", 1.0F);
     }
 }
