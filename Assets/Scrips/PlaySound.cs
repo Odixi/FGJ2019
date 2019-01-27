fdba6620b92a77607a5182772dc2f148432731b1
Odixi <vz4ev4r@gmail.com>
2019-01-27 15:10:25 +0200

    Kaukoimuriäänet

---
+++ Assets/Scrips/PlaySound.cs
@@ -0,0 +1,27 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySound : MonoBehaviour
+{
+    AudioSource audioSource;
+    public float time;
+    public float timeWindow = 0.01f;
+
+    private void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            if (TimeController.Instance.CurrentTime > time - timeWindow && TimeController.Instance.CurrentTime < time + timeWindow)
+            {
+                audioSource.Play();
+            }
+        }
+
+    }
+}
